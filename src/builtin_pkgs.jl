5cf96d5382d4957082d2e2ac27c73cce279f9ba6
Gunnar Farnebäck <gunnar.farneback@contextvision.se>
2021-11-28 00:17:24 +0100

    Fix for change in Julia master (#66)
    * Adapt to changes in output of Pkg.Types.stdlibs.
    
    * Bump the patch version number (since this is a bug fix)
    
    Co-authored-by: Dilum Aluthge <dilum@aluthge.com>

--- src/builtin_pkgs.jl
+++ src/builtin_pkgs.jl
@@ -1,2 +1,5 @@
 const stdlibs = isdefined(Pkg.Types, :stdlib) ? Pkg.Types.stdlib : Pkg.Types.stdlibs
-const BUILTIN_PKGS = Dict(v=>string(k) for (k, v) in stdlibs())
+# Julia 1.8 changed from `name` to `(name, version)`.
+get_stdlib_name(s::AbstractString) = s
+get_stdlib_name(s::Tuple) = first(s)
+const BUILTIN_PKGS = Dict(get_stdlib_name(v)=>string(k) for (k, v) in stdlibs())
