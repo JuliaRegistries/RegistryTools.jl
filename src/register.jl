0ecf5b9f9234cf20f883a2f46af29187fbfd86be
Nishanth H. Kottary <kottarynishanth54@gmail.com>
2022-05-18 12:56:11 +0530

    Log stacktrace to stderr (#69)
    * Log stacktrace to stderr
    
    * remove err and clean_registry

--- src/register.jl
+++ src/register.jl
@@ -653,8 +653,6 @@ function register(
         LibGit2.path(get_registry(GitTools.normalize_url(registry); gitconfig=gitconfig, force_reset=force_reset, cache=cache))
     end
 
-    clean_registry = true
-    err = nothing
     try
         # branch registry repo
         @debug("branch registry repo")
@@ -700,16 +698,11 @@ function register(
         else
             @debug("skipping git push")
         end
-
-        clean_registry = false
     catch ex
-        println(get_backtrace(ex))
+        @error("Unexpected error while registering", stacktrace=get_backtrace(ex))
         add!(status, :unexpected_registration_error)
-    finally
-        if clean_registry
-            @debug("cleaning up possibly inconsistent registry", registry_path=showsafe(registry_path), err=showsafe(err))
-            rm(registry_path; recursive=true, force=true)
-        end
+        @debug("cleaning up possibly inconsistent registry", registry_path=showsafe(registry_path))
+        rm(registry_path; recursive=true, force=true)
     end
     return set_metadata!(regbr, status)
 end
