3a8008ab70596594a1b14bf5d423b47c636243e7
Ian Butterworth <i.r.butterworth@gmail.com>
2023-08-27 15:26:16 -0400

    Add note about TagBot & branch format

--- src/utils.jl
+++ src/utils.jl
@@ -20,6 +20,8 @@ Generate the name for the registry branch used to register the package version.
 function registration_branch(pkg::Project; url::AbstractString)
     url_hash = bytes2hex(SHA.sha256(url))
     url_hash_trunc = url_hash[1:10]
+    # NOTE: If this format is changed TagBot must be updated
+    # https://github.com/JuliaRegistries/TagBot/blob/4e2dfa4ac8ad1e1a0af1e03f8411855200fac8ce/tagbot/action/repo.py#L219
     return "registrator-$(lowercase(pkg.name))-$(string(pkg.uuid)[1:8])-v$(pkg.version)-$(url_hash_trunc)"
 end
 function registration_branch(project_file::AbstractString; url::AbstractString)
