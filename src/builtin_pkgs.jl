3cb4e02739d0f98cb969c993b4a7a3c5ebac6ca9
Elliot Saba <staticfloat@gmail.com>
2024-06-03 13:30:48 -0700

    Adjust to https://github.com/JuliaLang/Pkg.jl/pull/3911

--- src/builtin_pkgs.jl
+++ src/builtin_pkgs.jl
@@ -2,4 +2,7 @@ const stdlibs = isdefined(Pkg.Types, :stdlib) ? Pkg.Types.stdlib : Pkg.Types.std
 # Julia 1.8 changed from `name` to `(name, version)`.
 get_stdlib_name(s::AbstractString) = s
 get_stdlib_name(s::Tuple) = first(s)
+if isdefined(Pkg.Types, :StdlibInfo)
+get_stdlib_name(info::Pkg.Types.StdlibInfo) = info.name
+end
 const BUILTIN_PKGS = Dict(get_stdlib_name(v)=>string(k) for (k, v) in stdlibs())
